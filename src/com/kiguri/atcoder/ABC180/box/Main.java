c4aee6ae0cbe46e89a2a31d0e2bf885c807c9bd2
ririkku <ryoji19950109@gmail.com>
2020-10-23 00:28:56 +0900

    解けた

---
+++ src/com/kiguri/atcoder/ABC180/box/Main.java
@@ -0,0 +1,18 @@
+package com.kiguri.atcoder.ABC180.box;
+
+import java.util.Scanner;
+
+/**
+ * https://atcoder.jp/contests/abc180/tasks/abc180_a
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println((a - b) + c);
+    }
+}
