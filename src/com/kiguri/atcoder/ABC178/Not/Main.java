a513a0c39a3b8b911d3817588c12e397cb380ba3
ririkku <ryoji19950109@gmail.com>
2020-10-26 21:05:31 +0900

    解けた

---
+++ src/com/kiguri/atcoder/ABC178/Not/Main.java
@@ -0,0 +1,19 @@
+package com.kiguri.atcoder.ABC178.Not;
+
+import java.util.Scanner;
+
+/**
+ * https://atcoder.jp/contests/abc178/tasks/abc178_a
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x == 0) {
+            System.out.println(1);
+        } else if (x == 1) {
+            System.out.println(0);
+        }
+    }
+}
