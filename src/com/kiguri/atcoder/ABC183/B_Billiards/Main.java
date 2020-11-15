7e2f477026b1739ed54640a3935c1de9dcbfa7c0
ririkku <ryoji19950109@gmail.com>
2020-11-16 00:18:23 +0900

    XXX

--- src/com/kiguri/atcoder/ABC183/B_Billiards/Main.java
+++ src/com/kiguri/atcoder/ABC183/B_Billiards/Main.java
@@ -1,11 +1,22 @@
-package com.kiguri.atcoder.ABC183.B_;
+package com.kiguri.atcoder.ABC183.B_Billiards;
+
+import java.util.Scanner;
 
 /**
- *
+ * https://atcoder.jp/contests/abc183/tasks/abc183_b
  */
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double Sx = sc.nextDouble();
+        double Sy = sc.nextDouble();
+        double Gx = sc.nextDouble();
+        double Gy = sc.nextDouble() * -1;
 
+        double v = (Gy - Sy) / (Gx - Sx);
+        double v1 = v * Sx - Sy;
+
+        System.out.println(v1 / v);
     }
 }
