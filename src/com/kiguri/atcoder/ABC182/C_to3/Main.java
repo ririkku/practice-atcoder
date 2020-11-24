2007a728d8ee3f67c3dfb46aef831cd891b5617f
ririkku <ryoji19950109@gmail.com>
2020-11-24 23:56:36 +0900

    諦め

---
+++ src/com/kiguri/atcoder/ABC182/C_to3/Main.java
@@ -0,0 +1,19 @@
+package com.kiguri.atcoder.ABC182.C_to3;
+
+import java.util.Scanner;
+
+/**
+ * https://atcoder.jp/contests/abc182/tasks/abc182_c
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+
+        for (int i = N.length(); i > 0; i--) {
+            String substring = N.substring(0, i);
+
+        }
+    }
+}
